30b40eb82d263b896e2f5d0f4036529d015c8096
Alexey Veremeenko <36800886+alverem@users.noreply.github.com>
2018-03-24 16:55:09 +0200

    Update task4_1.sh

--- task4_1.sh
+++ task4_1.sh
@@ -14,27 +14,19 @@ echo -e "\n">>task4_1.out
 #System Serial Number:
 
 	echo "System Serial Number:" `dmidecode -s system-serial-number`>>task4_1.out 
-#System Serial Number: XXXXXX
 
 echo "--- System ---">>task4_1.out
 
 	echo "OS Distribution:" `lsb_release -a | grep Des` >>task4_1.out 
-#xxxxx (например Ubuntu 16.04.4 LTS)
-
-	echo "Kernel version: " `uname -r`>>task4_1.out 
-
-#Kernel version: xxxx (например 4.4.0-116-generic)
-
-
+	echo "Kernel version: " `uname -r`>>task4_1.out
+#Installation date: xxxx	
 	echo "Installation date: "``>>task4_1.out
-#Installation date: xxxx
 
-	echo "Hostname: "`hostname`>>task4_1.out
 #Hostname: yyyyy
+	echo "Hostname: "`hostname`>>task4_1.out
+
 	echo "Uptime: "`uptime | sed 's/,.*//' | sed 's/.*up //'`>>task4_1.out
-#Uptime: XX days
 	echo "Processes running: "`ps aux | wc -l`>>task4_1.out
-#Processes running: 56684
 
 	echo "User logged in: "`who | wc -l`>>task4_1.out
 #User logged in: 665
@@ -47,9 +39,6 @@ a=`ip  link | grep "state UP" | awk '{print $2}'`
 echo -n $a "`ip addr show $a | grep "inet\b" | awk '{print $2}'`">>task4_1.out
 echo -e "\n">>task4_1.out
 
-
-
-#<Iface #2  name>:  IP/mask
 
 
 
