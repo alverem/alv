6da15f86c24fb2c92e04f765fbadccc724fd1b56
Alexey Veremeenko <36800886+alverem@users.noreply.github.com>
2018-03-25 23:46:09 +0300

    Update task4_1.sh

--- task4_1.sh
+++ task4_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-touch task4_1.out; chmod u+w task4_1.out
+touch task4_1.out; chmod 755 task4_1.out
 	echo "--- Hardware ---">>task4_1.out
 	echo "CPU" `cat /proc/cpuinfo | grep model\ name | uniq`>>task4_1.out
 
@@ -18,31 +18,19 @@ echo -e "\n">>task4_1.out
 echo "--- System ---">>task4_1.out
 
 	echo "OS Distribution:" `lsb_release -a | grep Des` >>task4_1.out 
-	echo "Kernel version: " `uname -r`>>task4_1.out
-#Installation date: xxxx	
-	echo "Installation date: "``>>task4_1.out
 
-#Hostname: yyyyy
+	echo "Kernel version: " `uname -r`>>task4_1.out 
+
+	echo "Installation date: " `ls -clt / | tail -n 1 | awk '{ print $7, $6, $8 }'`>>task4_1.out
+
 	echo "Hostname: "`hostname`>>task4_1.out
 
 	echo "Uptime: "`uptime | sed 's/,.*//' | sed 's/.*up //'`>>task4_1.out
+
 	echo "Processes running: "`ps aux | wc -l`>>task4_1.out
 
 	echo "User logged in: "`who | wc -l`>>task4_1.out
-#User logged in: 665
+
 	echo "--- Network ---">>task4_1.out
 
-#<Iface #1 name>:  IP/mask
-
-
-a=`ip  link | grep "state UP" | awk '{print $2}'`
-echo -n $a "`ip addr show $a | grep "inet\b" | awk '{print $2}'`">>task4_1.out
-echo -e "\n">>task4_1.out
-
-
-
-
-
-
-
-
+ip -o -f inet addr show | awk '{print $1" iface "$2 " : "  $4}'>>task4_1.out
